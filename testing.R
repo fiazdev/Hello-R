05159bd473dc66b70996eb9b61a2e1572d3c195b
fiazdev <fiaz9m2@gmail.com>
2020-12-29 03:50:49 +0500

    Few Lines Added

--- testing.R
+++ testing.R
@@ -1,2 +1,7 @@
 print("This was created with Rstudio")
 print ("And now it lives on Github")
+
+name="Fiaz"
+Version="Rstudio - In Writing"
+print(name)
+print("These line has been writtin in Rstudio")
